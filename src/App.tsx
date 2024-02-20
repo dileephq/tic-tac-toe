a8528e2526cc8238e267bb0d911fdeac442bfa38
dileephq <dileephq@gmail.com>
2024-02-21 05:16:15 +0530

    game layout fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,63 @@
 import './App.css'
 
+type GameStepProps = {
+  message: string
+}
+function GameStep({ message }: GameStepProps) {
+  const handleClick = () => {
+    console.log('clicked')
+  }
+  return (
+    <li onClick={handleClick} className="game-step">
+      {message}
+    </li>
+  )
+}
+
+type SquareProps = {
+  message: string
+}
+function Square({ message }: SquareProps) {
+  const handleClick = () => {
+    console.log('clicked')
+  }
+  return (
+    <div
+      onClick={handleClick}
+      className="square flex items-center justify-center"
+    >
+      <h2>{message}</h2>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="game ">
       <div>
         <h1 className="status">Next player: X</h1>
         <div>
-          <div className="square" />
-          <div className="square" />
-          <div className="square" />
+          <Square message="X" />
+          <Square message="O" />
+          <Square message="X" />
         </div>
         <div>
-          <div className="square" />
-          <div className="square" />
-          <div className="square" />
+          <Square message="X" />
+          <Square message="O" />
+          <Square message="X" />
         </div>
         <div>
-          <div className="square" />
-          <div className="square" />
-          <div className="square" />
+          <Square message="X" />
+          <Square message="O" />
+          <Square message="X" />
         </div>
       </div>
       <div className="game-info">
         <ol style={{ listStyle: 'auto' }}>
-          <li className="game-step">Go to Game start</li>
-          <li className="game-step">Go to move #1</li>
-          <li className="game-step">Go to move #2</li>
-          <li className="game-step">Go to move #3</li>
-          <li className="game-step">Go to move #3</li>
-          <li className="game-step">Go to move #3</li>
-          <li className="game-step">Go to move #3</li>
-          <li className="game-step">Go to move #3</li>
-          <li className="game-step">Go to move #3</li>
+          <GameStep message="Go to Game start" />
+          <GameStep message="Go to Step #1" />
+          <GameStep message="Go to Step #2" />
+          <GameStep message="Go to Step #3" />
         </ol>
       </div>
     </div>
